1acf242acda9aa2382b6e0fc57b26f935c36784a
Amitai Zand <amitai17@gmail.com>
2023-06-24 21:07:20 +0300

    Changed hero text and rounded offcanvas menu buttons more

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,7 +3,7 @@ function Hero() {
     return (
         <div id="hero" className="d-flex flex-column justify-content-center">
             <div id="hero-text-container-1" className="hero-text-container">
-                <h1 id="hero-text-1">I'm Amitai. Come fly with me!</h1>
+                <h1 id="hero-text-1">I'm Amitai. Take flight with me!</h1>
             </div>
             <div id="hero-text-container-2" className="hero-text-container">
                 <h2 id="hero-text-2">Full Stack Web Developer</h2>
